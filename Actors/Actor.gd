c0abe5b0f611052a5b18b8ac3d55f7019e759aa4
Christopher R. Palmer <crpalmer@gmail.com>
2023-01-28 19:53:41 -0500

    Working on normalizing the relationship between pixels / feet / speed / etc

--- Actors/Actor.gd
+++ Actors/Actor.gd
@@ -8,13 +8,14 @@ export var ac = 10
 export var hp = 1
 export var max_hp = 1
 export var to_hit_modifier = 0
-export var speed = 30
+export var speed_feet_per_round = 30
 export var xp_value = 1
 export var close_radius = 3
 export var vision_radius = 120
 export var mood = Mood.FRIENDLY
 export var next_action = 0
 
+var travel_distance_fudge_factor = 2
 var player_position
 var punch = load("%s/Actors/Punch.tscn" % GameEngine.config.root).instance()
 
@@ -128,8 +129,13 @@ func default_physics_process(delta):
 	if mood == Mood.HOSTILE and player_is_visible():
 		var dir:Vector2 = player_position - position
 		if dir.length() > 5:
-			var move_vector = dir.normalized() * GameEngine.real_time_to_game_time(delta) * GameEngine.feet_to_pixels(speed)
-			var _collision = move_and_collide(move_vector)
+			dir = dir.normalized()
+			var _collision = move_and_collide(dir * travel_distance_in_pixels(delta))
+
+func travel_distance_in_pixels(delta_elapsed_time):
+	var minutes = GameEngine.real_time_to_game_time(delta_elapsed_time)
+	var pixels_per_minute = GameEngine.feet_to_pixels(speed_feet_per_round)*6  # 10 rounds per minute
+	return pixels_per_minute * minutes / travel_distance_fudge_factor
 
 func _process(_delta):
 	if GameEngine.time_in_minutes < next_action: return
