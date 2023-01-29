a0643d4f067daddea9a3b055fe18d1a1d3843bd7
Christopher R. Palmer <crpalmer@gmail.com>
2023-01-28 20:52:54 -0500

    conversation redo: add missing files!!

---
+++ Actors/ConversationalActor.gd
@@ -0,0 +1,70 @@
+extends Actor
+class_name ConversationalActor
+
+export var seconds_per_interation = 15
+
+var player_is_in_area = false
+var in_conversation = false
+
+func get_persistent_data():
+	return .get_persistent_data()
+	
+func load_persistent_data(p):
+	.load_persistent_data(p)
+
+func name():
+	return display_name
+
+func start():
+	GameEngine.conversation.connect("player_said", self, "player_said")
+	GameEngine.conversation.start(name())
+	in_conversation = true
+	say_hello()
+
+func say_hello(): say("Hello.")
+func say_attacked(): say("Die!")
+func say_bye(): say("Bye.")
+
+func end(delay = 2.0):
+	GameEngine.conversation.disconnect("player_said", self, "player_said")
+	GameEngine.conversation.end(delay)
+	in_conversation = false
+
+func say(text):
+	GameEngine.conversation.say(text)
+	add_time()
+
+func say_in_parts(parts:Array):
+	GameEngine.conversation.say_in_parts(parts)
+	add_time()
+
+func add_time():
+	GameEngine.add_to_game_time(seconds_per_interation/60)
+
+func player_said(text:String, words:Array):
+	if "hi" in words:
+		say("Hello.")
+	elif "hello" in words:
+		say("Hi.")
+	elif "thanks" in words:
+		say("You're welcome.")
+	elif "name" in words:
+		say("My name is " + name())
+	elif "bye" in words or text == "":
+		say_bye()
+		end(0.75)
+	else:
+		say("I don't understand")
+
+func wants_to_initiate_conversation():
+	return false
+
+func _process(_delta):
+	if mood != Mood.FRIENDLY: return
+	var in_area = $CloseArea.player_is_in_area
+	if in_area == player_is_in_area: return
+	player_is_in_area = in_area
+	if not in_area and in_conversation:
+		end(0)
+	elif in_area and wants_to_initiate_conversation():
+		start()
