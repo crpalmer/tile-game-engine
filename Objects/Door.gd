8c6da17f08a598192fe2ec0b0050c205fea49abf
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-02 11:38:44 -0500

    door: improve the code

--- Objects/Door.gd
+++ Objects/Door.gd
@@ -5,17 +5,27 @@ export var is_locked = false
 export var is_closed = true
 export var key_group:String
 
+onready var open = get_node_or_null("Open")
+onready var closed = get_node_or_null("Closed")
+onready var lock = get_node_or_null("Lock")
+onready var blocker_open = get_node_or_null("BlockerOpen")
+onready var blocker_closed = get_node_or_null("BlockerClosed")
+
 func _ready():
 	ensure_state()
 
+func set_visibility(node, value):
+	if node: node.visible = value
+
+func set_disabled(node, value):
+	if node: node.disabled = value
+
 func ensure_state():
-	if has_node("Open"): $Open.visible = not is_closed
-	if has_node("Lock"): $Lock.visible = is_locked
-	if has_node("Closed"): $Closed.visible = is_closed
-	if has_node("BlockerOpen"): $BlockerOpen.disabled = is_closed
-	if has_node("BlockerClosed"): $BlockerClosed.disabled = not is_closed
-	for o in get_children():
-		if o is LightOccluder2D: o.visible = is_closed
+	set_visibility(open, not is_closed)
+	set_visibility(closed, is_closed)
+	set_visibility(lock, is_locked)
+	set_disabled(blocker_open, is_closed)
+	set_disabled(blocker_closed, not is_closed)
 	
 func used_by(who):
 	if who is Actor:
