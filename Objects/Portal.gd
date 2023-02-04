0d677101a0145dbddbd26e097546b7e0fd15c6e8
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-04 14:21:16 -0500

    portal: Use a FILE for the type of the scene to load

--- Objects/Portal.gd
+++ Objects/Portal.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-export var scene:String
-export var entry_point:String
+export(String, FILE) var scene
+export(String) var entry_point
 
 func _ready():
 	var _err = connect("body_entered", self, "body_entered")
