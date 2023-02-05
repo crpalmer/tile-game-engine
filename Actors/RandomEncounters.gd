c14f6853ff909e84871b80c1de2488c371d73650
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-05 16:44:33 -0500

    RandomEncounters: Add Area2D, randomize placement direction, check validity

--- Actors/RandomEncounters.gd
+++ Actors/RandomEncounters.gd
@@ -3,17 +3,22 @@ extends Node2D
 export(Array, String, FILE) var monsters
 export var check_every_hours = 24.0
 export var test_roll = 20
+export var area_extents = Vector2.ZERO
+onready var area = $Area2D
+onready var shape = $Area2D/CollisionShape2D
 
 var next_check_at = 0
+var player_in_area = 0
 
 func get_persistent_data():
 	var active = []
 	for c in get_children():
-		active.append({
-			"filename": c.filename,
-			"data": c.get_persistent_data(),
-			"global_position": c.global_position
-		})
+		if c is Actor:
+			active.append({
+				"filename": c.filename,
+				"data": c.get_persistent_data(),
+				"global_position": c.global_position
+			})
 	return {
 		"next_check_at": next_check_at,
 		"active": active
@@ -27,26 +32,46 @@ func load_persistent_data(p):
 
 func _ready():
 	add_to_group("PersistentOthers")
+	if area_extents != Vector2.ZERO:
+		shape.shape.extents = area_extents
+		area.connect("body_entered", self, "_on_body_entered")
+		area.connect("body_exited", self, "_on_body_exited")
 
 func set_next_check():
 	if monsters.size() > 0:
 		next_check_at = GameEngine.time_in_minutes + check_every_hours*60
 	else:
 		next_check_at = INF
-	print("Next random number check at %f in %f with %f + %f" % [next_check_at, next_check_at - GameEngine.time_in_minutes, GameEngine.time_in_minutes, check_every_hours*60])
 
 func place(m):
-	for x in [ 0, 1, -1]:
-		for y in [ -1, 1, 0]:
-			m.global_position = GameEngine.player.global_position + Vector2(x, y)*GameEngine.feet_to_pixels(5)
-			if m.player_is_in_sight():
-				return
+	var x_dir = [0, 1, -1]
+	var y_dir = [0, 1, -1]
+	x_dir.shuffle()
+	y_dir.shuffle()
+	for distance in [ 5, 3, 1]:
+		for x in x_dir:
+			for y in y_dir:
+				var place = Vector2(x, y)*GameEngine.feet_to_pixels(distance)
+				m.global_position = GameEngine.player.global_position + place
+				var collide = m.move_and_collide(-2*place, true, true, true)
+				if collide and collide.collider == GameEngine.player:
+					return true
+
+func allowed_to_generate():
+	if area_extents == Vector2.ZERO: return true
+	return player_in_area > 0
 
 func _physics_process(_delta):
 	if next_check_at == 0: set_next_check()
 	if GameEngine.time_in_minutes >= next_check_at:
 		set_next_check()
-		if GameEngine.roll_d20() >= test_roll:
+		if allowed_to_generate() and GameEngine.roll_d20() >= test_roll:
 			var m = load(monsters[randi() % monsters.size()]).instance()
 			add_child(m)
 			place(m)
+
+func _on_body_entered(body):
+	if body == GameEngine.player: player_in_area += 1
+
+func _on_body_exited(body):
+	if body == GameEngine.player: player_in_area -= 1
