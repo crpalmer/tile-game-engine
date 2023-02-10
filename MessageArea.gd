88fd3fe823e857251351edfc7e12994f6f76be54
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-09 20:49:30 -0500

    MessageArea: print a message when the player enters an area

---
+++ MessageArea.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+export(String, MULTILINE) var message
+export(bool) var one_shot = true
+
+var available = true
+
+func get_persistent_data():
+	return { "available": available }
+
+func load_persistent_data(p):
+	available = p.available
+
+func _ready():
+	var _err = connect("body_entered", self, "on_body_entered")
+	add_to_group("PersistentOthers")
+
+func on_body_entered(body):
+	if available and body == GameEngine.player:
+		GameEngine.message(message)
+		if one_shot: available = false
