6041bbb681e45b342b83d384e942e28db1c1b217
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-10 15:42:58 -0500

    portal: Only transport the player if it isn't paused
    Avoids what looks like a godot bug when entering a scene

--- Objects/Portal.gd
+++ Objects/Portal.gd
@@ -4,9 +4,8 @@ export(String, FILE) var scene
 export(String) var entry_point
 
 func _ready():
-	yield(get_tree(), "idle_frame")
 	var _err = connect("body_entered", self, "body_entered")
 
 func body_entered(body):
-	if body == GameEngine.player:
+	if not GameEngine.is_paused() and body == GameEngine.player:
 		GameEngine.call_deferred("enter_scene", scene, entry_point)
