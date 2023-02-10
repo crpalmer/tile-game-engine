6fe7047177c9ae36daaf74620c2781122571e0c6
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-09 20:52:13 -0500

    Portal: Add a wait for idle to _ready
    This gives the physics engine a chance to catch up with a scene
    change when we enter a new scene.

--- Objects/Portal.gd
+++ Objects/Portal.gd
@@ -4,8 +4,9 @@ export(String, FILE) var scene
 export(String) var entry_point
 
 func _ready():
+	yield(get_tree(), "idle_frame")
 	var _err = connect("body_entered", self, "body_entered")
 
 func body_entered(body):
-	if body is Player:
+	if body == GameEngine.player:
 		GameEngine.call_deferred("enter_scene", scene, entry_point)
