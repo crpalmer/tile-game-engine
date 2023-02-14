1c41e1f5e0518f8890c7f79303379a5ead2ace26
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-14 14:17:52 -0500

    Portal: optionally require a milestone before allowing the portal to work

--- Objects/Portal.gd
+++ Objects/Portal.gd
@@ -2,10 +2,14 @@ extends Area2D
 
 export(String, FILE) var scene
 export(String) var entry_point
+export(String) var required_milestone
+export(String) var milestone_needed_message
 
 func _ready():
 	var _err = connect("body_entered", self, "body_entered")
 
 func body_entered(body):
-	if not GameEngine.is_paused() and body == GameEngine.player:
+	if required_milestone != "" and not GameEngine.has_completed_milestone(required_milestone):
+		GameEngine.message(milestone_needed_message, true)
+	elif not GameEngine.is_paused() and body == GameEngine.player:
 		GameEngine.call_deferred("enter_scene", scene, entry_point)
