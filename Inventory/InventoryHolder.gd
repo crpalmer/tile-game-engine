3d1eaec624763d9ae601f3f3f3c358b2a032712f
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-14 11:16:30 -0500

    InventoryHolder: Add ability to look at the thing in your inventory

--- Inventory/InventoryHolder.gd
+++ Inventory/InventoryHolder.gd
@@ -8,6 +8,8 @@ export var is_equipped = false
 
 export(GameEngine.BodyParts, FLAGS) var requires
 
+var mouse_in_control = 0
+
 func get_persistent_data():
 	var thing = get_thing()
 	if thing:
@@ -19,11 +21,14 @@ func get_persistent_data():
 	else: return null
 
 func load_persistent_data(p):
-	GameEngine.instantiate(self, p.filename, p.data, p.global_position)
+	var thing = GameEngine.instantiate(self, p.filename, p.data)
+	set_thing_position(thing)
 
 func _ready():
 	add_to_group("InventoryHolders")
 	var _err = connect("inventory_changed", GameEngine.player, "on_inventory_changed")
+	_err = connect("mouse_entered", self, "on_mouse_entered")
+	_err = connect("mouse_exited", self, "on_mouse_exited")
 
 func get_thing():
 	for c in get_children():
@@ -34,11 +39,14 @@ func get_thing():
 func can_accept_thing(thing):
 	return not requires or (requires & thing.acceptable) != 0
 
+func set_thing_position(thing):
+	thing.position = rect_size/2
+
 func add_thing(thing):
 	if get_thing(): return false
 	if not can_accept_thing(thing): return false
 	if thing.get_parent(): thing.get_parent().remove_child(thing)
-	thing.position = rect_size/2
+	set_thing_position(thing)
 	add_child(thing)
 	updated(thing)
 	return true
@@ -67,3 +75,15 @@ func drop_data(_position, data):
 	data.thing.get_parent().remove_child(data.thing)
 	add_child(data.thing)
 	emit_signal("inventory_changed")
+
+func on_mouse_entered():
+	mouse_in_control += 1
+
+func on_mouse_exited():
+	mouse_in_control -= 1
+
+func _input(event:InputEvent):
+	if mouse_in_control > 0 and event.is_action_pressed("look"):
+		var thing = get_thing()
+		if thing and thing.description() != "":
+			GameEngine.message(thing.description())
