d44cc7a1fcbda13bde58c0fa3bed614c891baca7
Christopher R. Palmer <crpalmer@gmail.com>
2023-03-05 14:14:15 -0500

    Move findability back down to thing, but don't create the shape by default
    Instead, add a InventoryThingFindable as a convenience or the user can
    just add their own collision shape if they want because the name has
    been moved to a property.

--- Inventory/InventoryThing.gd
+++ Inventory/InventoryThing.gd
@@ -3,9 +3,6 @@ class_name InventoryThing
 
 @export var plural:String
 @export var group:String
-@export var reveal_text: String
-
-@export var findable: bool = true
 
 @export var combinable = false
 @export var value = 1
@@ -18,25 +15,19 @@ class_name InventoryThing
 
 @export var acceptable:GameEngine.BodyParts # (GameEngine.BodyParts, FLAGS)
 
-var findable_shape
-
 func get_persistent_data():
 	var p = super.get_persistent_data()
 	p.merge({
 		"n": n,
-		"visible": visible
 	})
 	return p
 
 func load_persistent_data(p):
 	super.load_persistent_data(p)
 	n = p.n
-	visible = p.visible
-	visibility_changed()
 
 func _ready():
 	super()
-	visibility_changed()
 	if plural == "": plural = display_name
 	add_to_group("InventoryThings")
 	add_to_group("Ephemeral")
@@ -52,13 +43,3 @@ func used_by(thing) -> bool:
 		if n > 0: reset_uses()
 		else: return true
 	return false
-
-func looked_at():
-	if not visible and findable:
-		if reveal_text != "": GameEngine.message(reveal_text, true)
-		visible = true
-		visibility_changed()
-
-func visibility_changed():
-	if findable_shape == null: 	findable_shape = $FindableCollisionShape2D
-	findable_shape.set_deferred("disabled", visible)
--- Objects/Thing.gd
+++ Objects/Thing.gd
@@ -3,6 +3,9 @@ class_name Thing
 
 @export var display_name:String : get = get_display_name
 @export_multiline var long_description:String
+@export var findable = true
+@export var reveal_text: String
+@export var findable_shape_path = "FindableCollisionShape2D"
 @export var max_uses:int = -1
 @export var use_time:float = 0.05
 @export var minutes_between_uses:float = 0.2
@@ -10,23 +13,30 @@ class_name Thing
 var next_use_at:float = 0
 var cur_uses:int
 
+var findable_shape
+
 func get_persistent_data():
 	return {
 		"next_use_at": next_use_at,
-		"cur_uses": cur_uses
+		"cur_uses": cur_uses,
+		"visible": visible
 	}
 
 func load_persistent_data(p):
 	next_use_at = p.next_use_at
 	cur_uses = p.cur_uses
+	if p.has("visible"): visible = p.visible
+	visibility_changed()
 
 func _ready():
 	add_to_group("PersistentNodes")
 	add_to_group("Trackables")
 	add_to_group("Things")
+	findable_shape = get_node_or_null(findable_shape_path)
 	if display_name == "": display_name = name
 	if minutes_between_uses < use_time: minutes_between_uses = use_time
 	reset_uses()
+	visibility_changed()
 
 func get_display_name() -> String:
 	if cur_uses > 1: return "%d uses of %s" % [ cur_uses, display_name ]
@@ -39,6 +49,15 @@ func used_by(_thing) -> bool:
 	if cur_uses > 0: cur_uses -= 1
 	next_use_at = GameEngine.time_in_minutes + minutes_between_uses
 	return cur_uses == 0
+
+func looked_at():
+	if not visible and findable:
+		if reveal_text != "": GameEngine.message(reveal_text, true)
+		visible = true
+		visibility_changed()
+
+func visibility_changed():
+	if findable_shape: findable_shape.set_deferred("disabled", visible)
 
 func capitalized_display_name() -> String:
 	var lower_case_name = get_display_name()
