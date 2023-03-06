208413d8dd7360a3445829ea7f3b778d8cb5b773
Christopher R. Palmer <crpalmer@gmail.com>
2023-03-06 10:07:02 -0500

    actor random movement: Handle navigation polygons as well
    As part of doing this, I ended up also optimizing away the repeated
    computation of the triangle areas.

--- Actors/actor_random_movement.gd
+++ Actors/actor_random_movement.gd
@@ -1,39 +1,45 @@
 extends Area2D
 class_name ActorRandomMovement
 
-@export var shape_node: String = "CollisionPolygon2D"
+@export var shape_node: String = "NavigationPolygon2D"
 @export var minimum_distance_percent = 50.0 # (float, 100)
 @export var only_see_player_when_in_area: bool = true
 @export var navigation_layer: int = 1
 
-@onready var start_position = global_position
-
+var start_position
 var actor
 var circle : CircleShape2D
 var rect : RectangleShape2D
-var polygon : CollisionPolygon2D
+var polygon : PackedVector2Array
 var triangles : PackedInt32Array
-var triangles_area
+var triangles_area = []
+var total_triangles_area
 var player_is_in_area = 0
 
 func _ready():
 	var _err = connect("body_entered",Callable(self,"body_entered"))
 	_err = connect("body_exited",Callable(self,"body_exited"))
 	var shape = get_node(shape_node)
+	start_position = shape.global_position
 	if shape is CollisionShape2D:
 		if shape.shape is CircleShape2D:
 			circle = shape.shape
 		elif shape.shape is RectangleShape2D:
 			rect = shape.shape
 	elif shape is CollisionPolygon2D:
-		polygon = shape
+		polygon = shape.polygon
+		prepare_polygon()
+	elif shape is NavigationRegion2D:
+		var navigation_polygon:NavigationPolygon = shape.navigation_polygon
+		assert(navigation_polygon.get_outline_count() == 1)
+		polygon = navigation_polygon.get_outline(0)
 		prepare_polygon()
 
 func may_see_player():
 	return not only_see_player_when_in_area or player_is_in_area > 0
 
 func triangle_area(t):
-	var p = polygon.polygon
+	var p = polygon
 	var p1 = p[triangles[t+0]]
 	var p2 = p[triangles[t+1]]
 	var p3 = p[triangles[t+2]]
@@ -45,32 +51,32 @@ func triangle_area(t):
 	return sqrt(s*(s-l1)*(s-l2)*(s-l3))
 
 func prepare_polygon():
-	triangles = Geometry2D.triangulate_polygon(polygon.polygon)
-	triangles_area = 0.0
+	triangles = Geometry2D.triangulate_polygon(polygon)
+	total_triangles_area = 0.0
 	for t in range(0, triangles.size(), 3):
-		triangles_area += triangle_area(t)
+		triangles_area.append(triangle_area(t))
+		total_triangles_area += triangle_area(t)
+	assert(triangles_area.size() == triangles.size() / 3)
 
 func pick_random_triangle():
-	var A = randf() * triangles_area
-	var t = 0
-	while t < polygon.polygon.size():
-		A -= triangle_area(t)
+	var A = randf() * total_triangles_area
+	for t in triangles_area.size():
+		A -= triangles_area[t]
 		if A <= 0: return t
-		t += 3
-	return t - 3
+	assert(0)
+	return randi() % triangles_area.size()
 
 func pick_random_point_in_triangle(t):
-	var p = polygon.polygon
-	var p1 = p[triangles[t+0]]
-	var p2 = p[triangles[t+1]]
-	var p3 = p[triangles[t+2]]
+	var p = polygon
+	var p1 = p[triangles[3*t+0]]
+	var p2 = p[triangles[3*t+1]]
+	var p3 = p[triangles[3*t+2]]
 	var r1 = randf()
 	var r2 = randf()
 	var point = Vector2.ZERO
 	point.x = (1 - sqrt(r1)) * p1.x + (sqrt(r1) * (1 - r2)) * p2.x + (sqrt(r1) * r2) * p3.x
 	point.y = (1 - sqrt(r1)) * p1.y + (sqrt(r1) * (1 - r2)) * p2.y + (sqrt(r1) * r2) * p3.y
-	point += start_position
-	return point
+	return point + start_position
 
 func get_persistent_data():
 	return {}
