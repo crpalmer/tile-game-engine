708113adac110149102b08c39d84897058f4f6a6
Christopher R. Palmer <crpalmer@gmail.com>
2023-03-08 06:15:31 -0500

    xp_area is really just a subset of message_area, unify them

--- message_area.gd
+++ message_area.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-@export_multiline var message
+@export_multiline var message = ""
 @export var is_important = false
 @export var one_shot = true
 @export var milestone_granted: String
+@export var xp:int = 0
 
 var available = true
 
@@ -19,6 +20,10 @@ func _ready():
 
 func on_body_entered(body):
 	if available and body == GameEngine.player:
-		GameEngine.message(message, is_important)
+		if message != "": GameEngine.message(message, is_important)
 		if milestone_granted != "": GameEngine.complete_milestone(milestone_granted)
 		if one_shot: available = false
+		if xp != 0:
+			if xp > 0: GameEngine.player.add_xp(xp)
+			if xp < 0: GameEngine.player.lose_xp(-xp)
+			xp = 0      # Only grant the XP once!
